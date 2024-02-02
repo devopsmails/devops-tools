46792e50cf393c5defca12d37a5068a2ab5d5603
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2024-02-02 16:21:41 +0530

    Create test.py

---
+++ 11_scripting/python_scripting/python_components/functions/test.py
@@ -0,0 +1,23 @@
+n1 = 10
+n2 = 5
+
+def add():
+    a = n1 + n2
+    print(a)
+
+def sub():
+    s = n1 - n2
+    print(s)
+
+def mul():
+    m = n1 * n2
+    print(m)
+
+def div():
+    d = n1 / n2
+    print(d)
+
+add()
+sub()
+mul()
+div()
